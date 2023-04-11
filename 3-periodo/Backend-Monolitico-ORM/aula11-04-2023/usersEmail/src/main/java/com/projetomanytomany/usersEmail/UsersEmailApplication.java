7cac7e06a9b1dcd8bba3517e5d2baa747e0cef92
aluno <aluno@udicentro.local>
2023-04-11 19:38:28 -0300

    dividindo o repositorio em periodos, materias e aulas

---
+++ 3-periodo/Backend-Monolitico-ORM/aula11-04-2023/usersEmail/src/main/java/com/projetomanytomany/usersEmail/UsersEmailApplication.java
@@ -0,0 +1,13 @@
+package com.projetomanytomany.usersEmail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersEmailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsersEmailApplication.class, args);
+	}
+
+}
---
+++ 3-periodo/Backend-Monolitico-ORM/aula11-04-2023/usersEmail/src/test/java/com/projetomanytomany/usersEmail/UsersEmailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetomanytomany.usersEmail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersEmailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
