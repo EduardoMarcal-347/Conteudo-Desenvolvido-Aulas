650a7eee41c73d1ddacfe5c0be49a927a03e9b26
EduardoMarcal-347 <edumsfilho347@gmail.com>
2023-04-12 20:07:18 -0300

    adicionando css inline

--- 3-periodo/JavaScript/aula12-04-2023/app01/src/TituloDepoimentos.jsx
+++ 3-periodo/JavaScript/aula12-04-2023/app01/src/TituloDepoimentos.jsx
@@ -4,7 +4,7 @@ export default function TituloDepoimentos(){
     
     return(
         <div>
-            <h1>Clientes Felizes. Novos Amigos :)</h1>
+            <h1 style={{color:"red", textAlign:"center"}}>Clientes Felizes. Novos Amigos :)</h1>
             <p>DEPOIMENTO DE ALGUNS CLIENTES REAIS</p>
         </div>
     )
