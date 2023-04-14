462f5165e92011b0a601c0948af796c5027cd0f7
EduardoMarcal-347 <edumsfilho347@gmail.com>
2023-04-14 20:48:36 -0300

    implementando classe Vendedor

---
+++ 3-periodo/Programacao-Orientada-Objeto/aula14-04-2023/atividade-interfaces/src/exercicio01/Vendedor.java
@@ -0,0 +1,28 @@
+package exercicio01;
+
+public class Vendedor extends Funcionario{
+
+    private Double comissao;
+
+    public Vendedor() {
+    }
+
+    public Vendedor(String nome, String matricula, Double salarioBase, Double comissao) {
+        super(nome, matricula, salarioBase);
+        this.comissao = comissao;
+    }
+
+    public Double getComissao() {
+        return comissao;
+    }
+
+    public void setComissao(Double comissao) {
+        this.comissao = comissao;
+    }
+
+    @Override
+    public Double calculaSalario() {
+        return getSalarioBase()+this.comissao;
+    }
+    
+}
