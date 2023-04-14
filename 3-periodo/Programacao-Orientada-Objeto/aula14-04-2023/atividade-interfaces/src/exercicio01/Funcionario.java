58055a6552dc8216ebbe7450fca23f95f9db3ad1
EduardoMarcal-347 <edumsfilho347@gmail.com>
2023-04-14 20:41:19 -0300

    inserindo atributos, construtores, getters e setters

--- 3-periodo/Programacao-Orientada-Objeto/aula14-04-2023/atividade-interfaces/src/exercicio01/Funcionario.java
+++ 3-periodo/Programacao-Orientada-Objeto/aula14-04-2023/atividade-interfaces/src/exercicio01/Funcionario.java
@@ -2,6 +2,44 @@ package exercicio01;
 
 public abstract class Funcionario {
 
+    private String nome;
+    private String matricula;
+    private Double salarioBase;
+
+    public Funcionario() {
+    }
+
+    public Funcionario(String nome, String matricula, Double salarioBase) {
+        this.nome = nome;
+        this.matricula = matricula;
+        this.salarioBase = salarioBase;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public Double getSalarioBase() {
+        return salarioBase;
+    }
+
+    public void setSalarioBase(Double salarioBase) {
+        this.salarioBase = salarioBase;
+    }
+
     public abstract Double calculaSalario();
+
 
 }
