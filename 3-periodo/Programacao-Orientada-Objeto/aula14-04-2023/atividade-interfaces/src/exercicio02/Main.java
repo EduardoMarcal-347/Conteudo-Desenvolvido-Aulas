7dcba170df10960ec684d958db9534b610fc9c3a
EduardoMarcal-347 <edumsfilho347@gmail.com>
2023-04-14 21:18:15 -0300

    implementando teste manual para cartoes

---
+++ 3-periodo/Programacao-Orientada-Objeto/aula14-04-2023/atividade-interfaces/src/exercicio02/Main.java
@@ -0,0 +1,27 @@
+package exercicio02;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<CartaoWeb> cartoes = new ArrayList<>();
+        Aniversario aniversario = new Aniversario("Eduardo");
+        DiaDosNamorados diaNamorados = new DiaDosNamorados("fulana");
+        Natal natal = new Natal("Tatiane");
+        cartoes.add(aniversario);
+        cartoes.add(diaNamorados);
+        cartoes.add(natal);
+        Iterator<CartaoWeb> iteratorCartoes = cartoes.iterator();
+        showCartoes(iteratorCartoes);
+    }
+
+    public static void showCartoes(Iterator<CartaoWeb> iterator){
+        iterator.next().showMessage();
+        if (iterator.hasNext()){
+            showCartoes(iterator);
+        }
+    }
+    
+}
