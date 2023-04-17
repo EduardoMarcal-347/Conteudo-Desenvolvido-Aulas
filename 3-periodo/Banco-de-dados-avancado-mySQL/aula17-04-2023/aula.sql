fab9605c485af25920af76f5d533c7bfe3068760
EduardoMarcal-347 <edumsfilho347@gmail.com>
2023-04-17 19:45:16 -0300

    procedimentos com replace

--- 3-periodo/Banco-de-dados-avancado-mySQL/aula17-04-2023/aula.sql
+++ 3-periodo/Banco-de-dados-avancado-mySQL/aula17-04-2023/aula.sql
@@ -10,3 +10,12 @@ DELIMITER ;
 
 Call brasil_cidade_inserir (16,"Tilambuco");
 
+DELIMITER $
+CREATE PROCEDURE brasil_cidade_atualizar (
+id_cidade int,
+id_estado int,
+nome varchar (50))
+BEGIN 
+	REPLACE INTO cidade VALUES(id_cidade, id_estado, nome);
+END $
+DELIMITER ;
