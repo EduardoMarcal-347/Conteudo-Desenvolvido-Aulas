58396495afae6edcebff0fe73f220573c4058917
EduardoMarcal-347 <edumsfilho347@gmail.com>
2023-04-17 20:12:43 -0300

    procedimentos com if e else

--- 3-periodo/Banco-de-dados-avancado-mySQL/aula17-04-2023/aula.sql
+++ 3-periodo/Banco-de-dados-avancado-mySQL/aula17-04-2023/aula.sql
@@ -1,4 +1,4 @@
-//Criando procedimentos
+ //Criando procedimentos
 
 DELIMITER $
 CREATE PROCEDURE brasil_cidade_inserir (id_estado int, nome varchar(50))
@@ -8,7 +8,7 @@ BEGIN
 END $
 DELIMITER ;
 
-Call brasil_cidade_inserir (16,"Tilambuco");
+call brasil_cidade_inserir (16,"Tilambuco");
 
 DELIMITER $
 CREATE PROCEDURE brasil_cidade_atualizar (
@@ -19,3 +19,19 @@ BEGIN
 	REPLACE INTO cidade VALUES(id_cidade, id_estado, nome);
 END $
 DELIMITER ;
+
+call brasil_cidade_atualizar (9715, 16, "TILAMBUCO");
+
+DELIMITER $
+CREATE PROCEDURE brasil_cidades_listar(ID_cidade int)BEGIN
+IF(id_cidade is null) THEN
+	SELECT * FROM cidade;
+ELSE
+	SELECT * FROM cidade
+	WHERE id = id_cidade;
+END IF;
+END $
+DELIMITER ;
+
+call brasil_cidades_listar(2);
+
