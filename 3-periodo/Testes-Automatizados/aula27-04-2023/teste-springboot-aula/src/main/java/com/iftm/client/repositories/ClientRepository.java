c7d8c7ad40f1b722c8684b8e04ac67646239bac0
EduardoMarcal-347 <edumsfilho347@gmail.com>
2023-04-28 19:21:58 -0300

    incluindo metodos findByIncome

--- 3-periodo/Testes-Automatizados/aula27-04-2023/teste-springboot-aula/src/main/java/com/iftm/client/repositories/ClientRepository.java
+++ 3-periodo/Testes-Automatizados/aula27-04-2023/teste-springboot-aula/src/main/java/com/iftm/client/repositories/ClientRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -12,8 +13,16 @@ public interface ClientRepository extends JpaRepository<Client, Long> {
     @Query(value = "SELECT c FROM Client c WHERE c.id = :id")
     public Optional<Client> findById(Long id);
 
-    @Query(value = "SELECT c FROM Client c WHERE c.name = :name")
+    @Query(value = "SELECT c FROM Client c WHERE LOWER(c.name) = LOWER(:name)")
     public Optional<Client> findByName(Long name);
+
+    @Query(value = "SELECT c FROM Client c WHERE c.income > :value")
+    public List<Client> findByIncomeBiggerThan(Double value);
+
+    @Query(value = "SELECT c FROM Client c WHERE c.income < :value")
+    public List<Client> findByIncomeSmallerThan(Double value);
+    @Query(value = "SELECT c FROM Client c WHERE c.income BETWEEN :value and :secondValue")
+    public List<Client> findByIncomeInterval(Double value, Double secondValue);
 
 
 
--- 3-periodo/Testes-Automatizados/aula27-04-2023/teste-springboot-aula/src/test/java/com/iftm/client/repositories/ClientRepositoryTest.java
+++ 3-periodo/Testes-Automatizados/aula27-04-2023/teste-springboot-aula/src/test/java/com/iftm/client/repositories/ClientRepositoryTest.java
@@ -14,26 +14,8 @@ public class ClientRepositoryTest {
     @Autowired
     private ClientRepository repositorio;
 
-    /**
-     * Cenário de Teste 1
-     * Objetivo: Verificar se a busca por id realmente retorna o cliente correto.
-     * ●
-     * monta o cenário,
-     * - arquivo import.sql carrega o cenário (clientes cadastrados)
-     * - definir o id de um cliente que exista em import.sql
-     * ●
-     * executa a ação
-     * - executar o método de buscar por id
-     * (Utilizaremos a classe Optional para encapsular o objeto da
-     * classe Client)
-     * ●
-     * e valida a saída.
-     * - verifica se foi retornado um objeto.
-     * - verifica se o nome e o cpf do cliente retornado corresponde
-     * ao esperado.
-     */
     @Test
-    @DisplayName("Verificar se a busca por id realmente retorna o\n" + "     * cliente correto.")
+    @DisplayName("Verificar se a busca por id realmente retorna o cliente correto.")
     public void testarBuscaPorIDRetornaClienteCorreto() {
         long idBuscado = 1; //corresponde ao primeiro registro do arquivo import.sql
         String nomeBuscado = "Conceição Evaristo";
@@ -43,35 +25,10 @@ public class ClientRepositoryTest {
 
         Optional<Client> resposta = repositorio.findById(idBuscado);
 
-        //verificação se um client foi retornado
-        Assertions.assertThat(resposta).isPresent();
-        //assertTrue(resposta.isPresent());
-        //assertNotNull(resposta.get());
-
-        //verificar se o objeto retorna corresponde ao objeto esperado.
-        //Assertions.assertThat(resposta.get()).isEqualTo(respostaEsperada);
-
-        //verificar se os dados do cliente esperado corresponde aos dados do cliente retornado
         Assertions.assertThat(resposta.get().getName()).isEqualTo(nomeBuscado);
         Assertions.assertThat(resposta.get().getCpf()).isEqualTo(cpfBuscado);
-        //assertEquals(nomeBuscado, resposta.get().getName());
-        //assertEquals(cpfBuscado, resposta.get().getCpf());
     }
 
-    /**
-     * Cenário de Teste 2
-     * Objetivo: Verificar se a busca por id inexistente retorna nenhum cliente.
-     * ●
-     * monta o cenário,
-     * - arquivo import.sql carrega o cenário (clientes cadastrados)
-     * - definir o id de um cliente que não exista em import.sql
-     * ●
-     * executa a ação
-     * - executar o método de buscar por id
-     * ●
-     * e valida a saída.
-     * - verifica se não foi retornado um objeto.
-     */
     @Test
     @DisplayName("Verificar se a busca por id inexistente retorna nenhum cliente")
     public void testarBuscaPorIdNaoRetornaObjetoParaIdInexistente() {
@@ -83,21 +40,6 @@ public class ClientRepositoryTest {
         //assertTrue(resultado.isEmpty());
     }
 
-    /**
-     * Cenário de Teste 3
-     * Objetivo: Verificar se a exclusão realmente apaga um registro existente.
-     * ●
-     * monta o cenário,
-     * - arquivo import.sql carrega o cenário (clientes cadastrados)
-     * - definir o id de um cliente que exista em import.sql
-     * ●
-     * executa a ação
-     * - executar o método de exclusão por id
-     * - executar o método de buscar por id
-     * ●
-     * e valida a saída.
-     * - verificar se o resultado do método de busca é falso
-     */
     @Test
     @DisplayName("Verificar se a exclusão realmente apaga um registro existente.")
     public void TestarExcluirPorIdApagaRegistroExistente() {
